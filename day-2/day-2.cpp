7a8290b5e5cc5ebbeb0a19385a9eb9505090e4e4
d0ugherty <doughe38@students.rowan.edu>
2023-12-09 23:56:07 -0500

    part one solution

--- day-2/day-2.cpp
+++ day-2/day-2.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <regex>
 #include <sstream>
 
 
@@ -24,60 +25,89 @@ std::vector<std::string> splitString(std::string str, std::string delim) {
 
 }
 
-
-
-bool isValidGame(std::string line) {
+int extractInt(std::string str){
     std::stringstream ss;
-    std::vector<int> cubes;
+    ss << str;
+    std::string temp;
+    int result;
+    int testNumber;
+
+    while (!ss.eof()) {
+        ss >> temp;
+            // if the conversion from a string to a number is successful,
+            //  then we've found an integer in the string
+        if (std::stringstream(temp) >> testNumber) {
+            result = testNumber; 
+        }
+    }
+    return result;
+}
+
+bool isValidGame(std::vector<std::string> tokens) {
+    int cubes;
+    std::string red = "\\d{1,2} red";
+    std::string green = "\\d{1,2} green";
+    std::string blue =  "\\d{1,2} blue";
+
+    std::regex red_regex(red);
+    std::regex green_regex(green);
+    std::regex blue_regex(blue);
+    std::smatch match;
+
     const int redCubes = 12;
     const int greenCubes = 13;
     const int blueCubes = 14;
-    // store string in string stream
-    ss << line;
-    std::string temp;
-    int test_num;
     
-    while (!ss.eof()) {
+    for(std::string token : tokens) {
         
-        ss >> temp;
-        // if the the content of the string stream is successfully converted
-        // to an integer, then we've found a number
-        if(std::stringstream(temp) >> test_num) {
-            cubes.push_back(test_num);
+        if(std::regex_search(token, match, red_regex)){
+            cubes = extractInt(match[0]);
+            if(cubes > redCubes){
+                return false;
+            }
+        }
+
+        if(std::regex_search(token, match, green_regex)){
+            cubes = extractInt(match[0]);
+            if(cubes > greenCubes) {
+                return false;
+            }
+        }
+
+        if(std::regex_search(token, match, blue_regex)) {
+            cubes = extractInt(match[0]);
+            if(cubes > blueCubes) {
+                return false;
+            }
         }
     }
-    
-
-    
     return true;
 }
+
 
 int main () {
     std::string line;
     std::ifstream infile("input.txt");
     std::vector<std::string> split_line;
-    int validGames;
+    int sumOfGames;
 
-    validGames = 0;
-    
+    sumOfGames = 0;
+    int gameId = 1;
+
     while(std::getline(infile,line)){
         // Drop the "Game NN:" portion of the line
         size_t pos = line.find(":") + 1;
         line = line.substr(pos);
-        // std::cout << str << std::endl;
         // Break up each line (game) into invidual rounds of the game
         std::vector<std::string> tokens = splitString(line, ";");
         
-        if(isValidGame(line)) {
-            validGames++;
-         }
-
-        for(std::string token : tokens) {
-            std::cout << token << std::endl;
+        if(isValidGame(tokens)) {
+            sumOfGames += gameId;
         }
+        gameId++;
     }
     
-    std::cout << "Number of games: " << validGames << std::endl;
+    std::cout << "Number of games: " << sumOfGames << std::endl;
 
     return 0;
 }
