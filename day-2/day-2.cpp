a9e55b2c8c090a58aabd6ab0734526680c460376
d0ugherty <tdougherty16@gmail.com>
2023-12-09 22:22:21 -0500

    moving to desktop

--- day-2/day-2.cpp
+++ day-2/day-2.cpp
@@ -4,17 +4,13 @@
 #include <sstream>
 
 
-std::vector<std::string> split_string(std::string str, std::string delim) {
+std::vector<std::string> splitString(std::string str, std::string delim) {
     std::string token;
     std::vector<std::string> result;
     size_t end_pos;
     size_t delim_length = delim.length();
     size_t start_pos = 0;
     
-    // Drop the "Game NN:" portion of the line
-    size_t pos = str.find(":") + 1;
-    str = str.substr(pos);
-    std::cout << str << std::endl;
 
     while ((end_pos = str.find(delim, start_pos)) != std::string::npos) {
         token = str.substr(start_pos, end_pos - start_pos);
@@ -28,10 +24,31 @@ std::vector<std::string> split_string(std::string str, std::string delim) {
 
 }
 
+
+
 bool isValidGame(std::string line) {
+    std::stringstream ss;
+    std::vector<int> cubes;
+    const int redCubes = 12;
+    const int greenCubes = 13;
+    const int blueCubes = 14;
+    // store string in string stream
+    ss << line;
+    std::string temp;
+    int test_num;
+    
+    while (!ss.eof()) {
+        
+        ss >> temp;
+        // if the the content of the string stream is successfully converted
+        // to an integer, then we've found a number
+        if(std::stringstream(temp) >> test_num) {
+            cubes.push_back(test_num);
+        }
+    }
     
 
-
+    
     return true;
 }
 
@@ -39,23 +56,24 @@ int main () {
     std::string line;
     std::ifstream infile("input.txt");
     std::vector<std::string> split_line;
-    const int redCubes = 12;
-    const int greenCubes = 13;
-    const int blueCubes = 14;
     int validGames;
 
     validGames = 0;
     
     while(std::getline(infile,line)){
+        // Drop the "Game NN:" portion of the line
+        size_t pos = line.find(":") + 1;
+        line = line.substr(pos);
+        // std::cout << str << std::endl;
         // Break up each line (game) into invidual rounds of the game
-        split_line = split_string(line, ";");
+        std::vector<std::string> tokens = splitString(line, ";");
         
         if(isValidGame(line)) {
             validGames++;
          }
 
-        for(std::string token : split_line) {
-            //std::cout << token << std::endl;
+        for(std::string token : tokens) {
+            std::cout << token << std::endl;
         }
     }
     
