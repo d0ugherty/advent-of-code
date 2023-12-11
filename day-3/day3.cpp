8bcdac4e84a98e4c4208370a3d2c3707992e328b
d0ugherty <doughe38@students.rowan.edu>
2023-12-10 23:09:02 -0500

    begin searching for part number - will return later

--- day-3/day3.cpp
+++ day-3/day3.cpp
@@ -4,16 +4,38 @@
 #include <string>
 #include <vector>
 
-
-int sumOfPartNumbers(std::vector<std::string>& fileArray){
-    return 0;
-}
-
 void print(std::string output) {
     std::cout << output << std::endl;
 }
 
-std::vector<std::string> toArray2D(std::istream& infile){
+bool isPartNumber(){
+    return true;
+}
+
+int searchForPart(std::string currLine, std::string prevLine = "", std::string nextLine = "") {
+    size_t size = currLine.size() - 1;
+    for(int i = 0; i <= size; i++) {
+        
+    }
+}
+
+int sumOfPartNumbers(std::vector<std::string>& fileArray){
+    int sum;
+    int partNumber;
+    size_t size = fileArray.size() - 1;
+
+    sum = 0;
+    for(int i = 0; i <= size; i++){
+        std::string currLine = fileArray[i];
+        std::string nextLine = i != size ? fileArray[i+1] : "";
+        std::string prevLine = i != 0 ? fileArray[i-1] : "";
+
+        sum += searchForPart(currLine, prevLine, nextLine);       
+    }
+    return sum;
+}
+
+std::vector<std::string> toArray(std::istream& infile){
     std::vector<std::string> result;
     std::string inputLine;
 
@@ -22,11 +44,8 @@ std::vector<std::string> toArray2D(std::istream& infile){
         std::string arrayLine;
         while(ss >> arrayLine) {
             result.push_back(arrayLine);
-            print(arrayLine);
         }
-
         result.push_back("\n");
-        print("\n");
     }
     return result;
 }
@@ -46,6 +65,6 @@ int main(int argc, char* argv[]) {
     int sumOfPartNumbers;
     // reference to the stream
     std::istream& input = *input_ptr;
-    fileArray = toArray2D(input);
+    fileArray = toArray(input);
     return 0;
 }
